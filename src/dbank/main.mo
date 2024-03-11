00a7f4212b218e713fd16bdb52a1f12246649dda
fahadkabali <kabalifahad@gmail.com>
2024-03-11 22:37:04 +0300

    update

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,33 +1,56 @@
-import Nat "mo:base/Nat";
 import Debug "mo:base/Debug";
-import Int "mo:base/Int";
 import Time "mo:base/Time";
 import Float "mo:base/Float";
-actor DBank{
-  stable var currentValue:Float =300;
+
+actor DBank {
+  stable var currentValue: Float = 300;
+  currentValue := 300;
+  Debug.print(debug_show(currentValue));
+
   stable var startTime = Time.now();
-  // currentValue :=100;
-  public func topUp(amount:Float){
+  startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  public func topUp(amount: Float) {
     currentValue += amount;
     Debug.print(debug_show(currentValue));
   };
-  public func withdraw(amount:Float){
-    let tempValue:Float = currentValue -amount;
+
+  public func withdraw(amount: Float) {
+    let tempValue: Float = currentValue - amount;
     if (tempValue >= 0) {
       currentValue -= amount;
       Debug.print(debug_show(currentValue));
     } else {
-      Debug.print("Insufficient funds");
-    };
+      Debug.print("Insufficient FUnds")
+    }
   };
-  public query  func checkBalance() : async Float{
-     return currentValue;
+
+  public query func checkBalance(): async Float {
+    return currentValue;
   };
-  public func compound(){
+
+  public func compound() {
     let currentTime = Time.now();
-    let elapsedTimeNS = currentTime - startTime; 
-    let elapsedTimeS= elapsedTimeNS / 1000000000;
-    currentValue := currentValue * (1.01 ** Float.fromInt(elapsedTimeS));
-    startTime:= currentTime;
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+    startTime := currentTime;
   };
-};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,29 +1,39 @@
 import { dbank } from "../../declarations/dbank";
 
-window.addEventListener("laod", async ()=>{
-    update();
+window.addEventListener("load", async function() {
+  update();
 });
-document.querySelector("form").addEventListener("submit",async (event)=> {
-    event.preventDefault();
-    const inputAmount = parseFloat(document.getElementById('input-amount').value);
-    const outputAmount = parseFloat(document.getElementById('withdrawal-amount').value)
-    button.setAttribute("disabled", true);
 
-    if(document.getElementById("input-amount").value.length !=0){
-        await  dbank.topUp(inputAmount);
-    }
-    if(document.getElementById("withdrawal-amount").value.length !=0){
-        await dbank.withdraw(outputAmount);
-    }
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
 
-    update();
-    document.getElementById( 'input-amount' ).value= "";
-    document.getElementById( 'withdrawal-amount' ).value="";
-    
-    button.removeAttribute("disabled")
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true);
+
+  if (document.getElementById("input-amount").value.length != 0) {
+    await dbank.topUp(inputAmount);
+  }
+
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await dbank.withdraw(outputAmount);
+  }
+
+  await dbank.compound();
+
+  update()
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+
+  button.removeAttribute("disabled");
 
 });
-async function update(){
-    const currentAmount = await dbank.checkBalance();
-    document.getElementById("value").innerText = Math.round(currentAmount*100)/100;
-}
+
+async function update() {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+};
