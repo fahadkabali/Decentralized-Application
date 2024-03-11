5106c3020eeead6022b85b124bc17ad348096390
fahadkabali <kabalifahad@gmail.com>
2024-03-11 22:23:30 +0300

    update

---
+++ src/dbank_assets/src/index.js
@@ -0,0 +1,29 @@
+import { dbank } from "../../declarations/dbank";
+
+window.addEventListener("laod", async ()=>{
+    update();
+});
+document.querySelector("form").addEventListener("submit",async (event)=> {
+    event.preventDefault();
+    const inputAmount = parseFloat(document.getElementById('input-amount').value);
+    const outputAmount = parseFloat(document.getElementById('withdrawal-amount').value)
+    button.setAttribute("disabled", true);
+
+    if(document.getElementById("input-amount").value.length !=0){
+        await  dbank.topUp(inputAmount);
+    }
+    if(document.getElementById("withdrawal-amount").value.length !=0){
+        await dbank.withdraw(outputAmount);
+    }
+
+    update();
+    document.getElementById( 'input-amount' ).value= "";
+    document.getElementById( 'withdrawal-amount' ).value="";
+    
+    button.removeAttribute("disabled")
+
+});
+async function update(){
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount*100)/100;
+}
